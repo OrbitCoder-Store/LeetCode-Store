08031818eef9e60d27c6e4fcf4d3b261da576717
OrbitCoder-Store <adityaindana@gmail.com>
2025-10-23 22:41:08 +0530

    Time: 77 ms (67.57%), Space: 18 MB (26.37%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasSameDigits(self, s: str) -> bool:
+        a = list(s)
+        for i in range(len(a) - 1, 1, -1):
+            for j in range(i):
+                a[j] = str((int(a[j]) + int(a[j + 1])) % 10)
+        return a[0] == a[1]
